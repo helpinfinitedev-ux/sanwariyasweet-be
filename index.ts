10d8d3123cf14c7a2b988b37070e2400ee1bdca0
Muhammad Faisal <faisalmd25121999@gmail.com>
2026-02-23 16:21:46 +0530

    changes

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import app from "./app";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 
 dotenv.config();
 
+// Connect to MongoDB before handling requests
+let isConnected = false;
+
+const handler = async (req: any, res: any) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+};
+
 // Export for Vercel serverless
-export default app;
+export default handler;
