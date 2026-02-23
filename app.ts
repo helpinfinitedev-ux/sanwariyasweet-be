3f26d4e9378ba934b77fc304c8f24f99924d080c
Muhammad Faisal <faisalmd25121999@gmail.com>
2026-02-23 16:14:39 +0530

    changes

---
+++ app.ts
@@ -0,0 +1,23 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRouter from "./routes/product.routes";
+import categoryRouter from "./routes/category.routes";
+import authRouter from "./routes/auth.routes";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT: string | number = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/products", productRouter);
+app.use("/categories", categoryRouter);
+app.use("/auth", authRouter);
+
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
